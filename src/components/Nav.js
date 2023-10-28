887b1a8d6ab00baba760d5b54d2e65e493359392
Hossain <arafat.kd99@gmail.com>
2023-10-28 19:09:20 +0100

    Fix linters error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import userIcon from '../Assets/user-regular.svg';
 
 const Nav = () => (
   <nav>
@@ -9,7 +8,6 @@ const Nav = () => (
       <li><Link to="/">BOOKS</Link></li>
       <li><Link to="/categories">CATEGORIES</Link></li>
     </ul>
-    {/* <a to="/"><userIcon /></a> */}
   </nav>
 );
 
