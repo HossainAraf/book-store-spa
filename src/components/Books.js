d0db9adc4f4c226452f01e0ddf7a65611f5d62eb
Hossain <arafat.kd99@gmail.com>
2023-10-28 18:47:26 +0100

    Update Books component

---
+++ src/components/Books.js
@@ -0,0 +1,15 @@
+// This component is used as Homepage
+
+import React from 'react';
+// import Book from './Book';
+import BookList from './BookList';
+import AddBook from './AddBook';
+
+const Books = () => (
+  <>
+    <BookList />
+    <AddBook />
+  </>
+);
+
+export default Books;
