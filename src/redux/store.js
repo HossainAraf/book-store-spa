b59d2d533995167af0ea2ad2b1e02d67d26dcac9
Hossain <arafat.kd99@gmail.com>
2023-11-02 19:09:19 +0100

    Combine reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,3 +2,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
+
+const reducer = combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer,
+});
+
+
