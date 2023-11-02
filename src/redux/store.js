b77bf8876e99fd83a724c785f3ce283ee98ae3ee
Hossain <arafat.kd99@gmail.com>
2023-11-02 19:06:39 +0100

    import booksReducer & categoriesReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,4 @@
 // redux/store.js
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
