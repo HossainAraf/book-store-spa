5dbdf2dd519bd00e93ce99fe3ea8317ef9da7236
Hossain <arafat.kd99@gmail.com>
2023-11-03 07:49:34 +0100

    Update initialState

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,9 +2,26 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 // STATE
-const initialBooksState = {
-  books: [],
-};
+const initialBooksState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 // CREATE A SLICE USING createSlice
 const bookSlice = createSlice({
