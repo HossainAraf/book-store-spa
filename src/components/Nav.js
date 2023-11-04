f765715e4884edb521c694cbabc205a2df433bd7
Hossain <arafat.kd99@gmail.com>
2023-11-04 21:00:10 +0100

    Update <a> element color

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,8 @@ const Nav = () => (
   <nav>
     <h1>Bookstore CMS</h1>
     <ul className={Nav.ul}>
-      <li id="nav-book"><Link to="/">BOOKS</Link></li>
-      <li id="nav-categories"><Link to="/categories">CATEGORIES</Link></li>
+      <li id="nav-book"><Link className="a-book" to="/">BOOKS</Link></li>
+      <li id="nav-categories"><Link className="a-categories" to="/categories">CATEGORIES</Link></li>
     </ul>
     <div id="user-icon">
       <FontAwesomeIcon icon={faUser} style={{ color: '#0290ff' }} />
