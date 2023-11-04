a5b99997a6af606a72e464dc7418b65ac3631a38
Hossain <arafat.kd99@gmail.com>
2023-11-04 19:14:36 +0100

    Add html elements & classNames to Books.jsx

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
 import { RemoveBookButton } from './Button';
 import '../styles/book.css';
-
+import progress from '../Assets/progress.png';
 
 const Book = ({
   id, title, author, category,
@@ -17,19 +17,43 @@ const Book = ({
   };
   return (
     <div className="book">
-      <h3>
-        Title:
-        {title}
-      </h3>
-      <p>
-        Author:
-        {author}
-      </p>
-      <p>
-        Category:
-        {category}
-      </p>
-      <RemoveBookButton type="button" onClick={handleRemoveBook}>Delete</RemoveBookButton>
+      <div className="book-cover">
+        <div className="book-info">
+          <h6 className="category">
+            {category}
+            Action
+          </h6>
+          <h3 className="title">
+            {title}
+          </h3>
+          <p className="author">
+            {author}
+          </p>
+        </div>
+        <div className="book-actions">
+          <p className="comments">
+            Comments
+          </p>
+          <span className="remove">
+            <RemoveBookButton className="remove-btn" type="button" onClick={handleRemoveBook}>Remove</RemoveBookButton>
+          </span>
+          <p className="edit">
+            Edit
+          </p>
+        </div>
+      </div>
+      <div className="progress">
+        <img src={progress} alt="progress" />
+        <div className="progress-info">
+          <p className="parcent">64%</p>
+          <p className="completed">Completed</p>
+        </div>
+      </div>
+      <div className="chapter">
+        <p className="current">CURRENT CHAPTER</p>
+        <p className="chapter-number">Chapter 17</p>
+        <button type="button" className="update-progress"><p>UPDATE PROGRESS</p></button>
+      </div>
     </div>
   );
 };
