2de6d0970018955bd2676e622c56435592e036ed
Hossain <arafat.kd99@gmail.com>
2023-11-04 10:27:48 +0100

    Update  book slice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -27,19 +27,75 @@ const initialState = {
 };
 
 // CREATE A SLICE USING createSlice
-const bookSlice = createSlice({
-  name: 'book',
-  initialState: initialBooksState,
-  reducers: {
-    addBook: (state, action) => {
-      state.push(action.payload);
-    },
-    removeBook: (state, action) => (
-      state.filter((book) => book.item_id !== action.payload)
-    ),
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.status = 'fetched';
+        if (action.payload !== '') {
+          const books = [];
+          const keys = Object.keys(action.payload);
+          keys.forEach((bookId) => {
+            books.push({ item_id: bookId, ...action.payload[bookId][0] });
+          });
+          state.books = books;
+          if (state.books.length === 0) {
+            state.error = 'No books found';
+          }
+        } else {
+          state.error = 'No books found';
+        }
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(addBook.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(addBook.fulfilled, (state, action) => {
+        if (action.payload !== null) {
+          state.status = 'succeeded';
+          state.error = '';
+          state.books.push(action.payload);
+        } else {
+          state.status = 'failed';
+          state.error = 'Unable to add book';
+        }
+      })
+      .addCase(addBook.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(removeBook.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(removeBook.fulfilled, (state, action) => {
+        if (action.payload !== null) {
+          state.status = 'succeeded';
+          state.error = '';
+          state.books = state.books.filter((bookId) => bookId.item_id !== action.payload);
+          if (state.books.length === 0) state.error = 'No book was found!';
+        } else {
+          state.status = 'failed';
+          state.error = 'Unable to remove book';
+        }
+      })
+      .addCase(removeBook.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
   },
 });
 
-// EXPORT ACTIONS & REDUCER
-export const { addBook, removeBook } = bookSlice.actions;
-export default bookSlice.reducer;
+
